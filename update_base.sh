0c8347271cdeeca4b32f3b848f75e7e8b15532fa
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2015-11-10 22:53:44 -0200

    Arquivo de envio

---
+++ update_base.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Script de update automatico de bases para respositórios no git
+
+#OBS: Use sh nome.sh para executar este arquivo
+#OBS2: Esse arquivo deve estar dentro da pasta (raiz) do repositório da sua base
+
+#Gera o nome do arquivo (data de hoje separando por '_')
+name_file=`date +%d_%m_%Y`
+
+#Conecta com o mysql e faz dump da base (já com o nome do arquivo da linha anterior)
+mysqldump nome_do_seu_banco > $name_file.sql -u seu_usuario_aqui -p sua_senha_aqui
+
+#Gera o nome da pasta baseado no mês e ano atual (padrão)
+name_dir=`date +%m_%Y`
+
+#Cria a pasta correspondente
+mkdir $name_dir
+
+#Move o arquivo criado para a pasta criada
+mv $name_file.sql $name_dir/
+
+
+#Comandos do git para enviar a base
+git add .
+
+git commit -m "Dump da base - Gerado Via Script"
+
+git push origin master
